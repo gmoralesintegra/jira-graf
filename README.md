91fdfd8a2a76c983e0457df1bbf680c007a544e8
gmoralesintegra <58697321+gmoralesintegra@users.noreply.github.com>
2020-01-29 21:08:28 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# jira-graf
-Integration jira on datasources of Grafana
+# Jira Grafana JSON Datasource Plugin
+### Connect Grafana to Jira Cloud to retrieve metrics on your Jira issues.
+
+The original intention for this project was to allow me to show a singlestat count of open issues for my team's Jira Service Desk.
+
+![Example](https://preview.ibb.co/hD2txU/screenshot_2018_09_23_094255.png)
+
+## Requirements
+This project only requires your Grafana instance to have the [SimpleJson plugin](https://grafana.com/plugins/grafana-simple-json-datasource/installation) installed.
+
+## Installation
+
+Run the Docker container (replace environment variables with your Jira Cloud credentials):
+```bash
+$ docker run -d -p 3005:3005 --name jira-grafana -e JIRA_HOST=myjira.atlassian.net -e JIRA_USER=mike@bluefrog.ca -e JIRA_PASS=XXXX bluefrg/jira-grafana-json-datasource
+```
+
+Confirm running by visiting: http://localhost:3005
+
+*Note: This project has only been tested locally, and as a ECS task using Jira Cloud. Jira On-Prem is not supported by the dependencies used in this project.*
+
+### Atlassian credentials
+It's recommended to use API tokens from your [Atlassian API token page](https://id.atlassian.com/manage/api-tokens) for authentication instead of your primary account username and password.
+
+### Testing Jira connection
+You can validate the connection to Jira is authenticating properly by visiting the `test-jira` endpoint at: http://localhost:3005/test-jira
+
+A successful authentication will return the current user, a failure will return the HTTP response and detailed error message from Jira.
+
+### docker-compose.yml
+The docker-compose is included as an example of how to test Grafana and  the Jira Grafana JSON Datasource integration. Not intended for production use.
+
+## Setup
+To add this as a data source in Grafana, go to Configuration -> Data Sources and click Add. For the Type choose SimpleJson. Enter the root URL for this project (e.g. http://localhost:3005).
+
+### Authentication
+You can require authentication by adding a `HTTP_USER` and `HTTP_PASS` environment variable.
+
+You will need to reconfigure your data source to use "Basic Auth".
+
+### DASHBOARD explain
+When adding a panel to a dashboard, choose the newly created data source. Under the metrics tab, you will see your Jira filters as an option to plot on your panel.
+
+![Example](https://thumb.ibb.co/fxS7rp/screenshot_2018_09_23_093243.png)
+
+## License
+
+This project is licensed under the MIT license. See the [LICENSE](LICENSE) file for more info.
---
+++ server.js
@@ -0,0 +1,174 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const JiraClient = require('jira-connector')
+const morgan = require('morgan')
+const passport = require('passport')
+const BasicStrategy = require('passport-http').BasicStrategy
+const AnonymousStrategy = require('passport-anonymous')
+
+
+// Instantiate our Jira client
+const jira = new JiraClient({
+  host: process.env.JIRA_HOST,
+  basic_auth: {
+    username: process.env.JIRA_USER,
+    password: process.env.JIRA_PASS
+  }
+})
+
+
+// Setup an authentication strategy
+let authenticationStrategy = null
+if (process.env.HTTP_USER) {
+  passport.use(new BasicStrategy(
+    function (username, password, done) {
+
+        if (process.env.HTTP_USER == username &&
+           process.env.HTTP_PASS == password) {
+          return done(null, true)
+        }
+
+      return done(null, false)
+    }
+  ))
+
+  authenticationStrategy = 'basic'
+}
+else {
+  // Default ot allowing anonymous access
+  passport.use(new AnonymousStrategy())
+  authenticationStrategy = 'anonymous'
+}
+
+
+app.use(bodyParser.json())
+app.use(morgan('combined')) // We want to log all HTTP requests
+app.use(passport.initialize())
+
+
+// Should return 200 ok. Used for "Test connection" on the datasource config page.
+app.get('/',
+  passport.authenticate(authenticationStrategy, { session: false }),
+  (httpReq, httpRes) => {
+    httpRes.set('Content-Type', 'text/plain')
+    httpRes.send(new Date() + ': OK')
+})
+
+
+// Test the connection between Jira and this project
+app.get('/test-jira',
+  passport.authenticate(authenticationStrategy, { session: false }),
+  (httpReq, httpRes) => {
+    
+    jira.myself.getMyself().then((jiraRes) => {
+      httpRes.json(jiraRes)
+    }).catch((jiraErr) => {
+      httpRes.json(JSON.parse(jiraErr))
+    })
+
+  })
+
+
+// Used by the find metric options on the query tab in panels.
+app.all('/search', 
+  passport.authenticate(authenticationStrategy, { session: false }),
+  (httpReq, httpRes) => {
+
+  // The JiraClient doesn't have any way to list filters so we need to do a custom query
+  jira.makeRequest({
+    uri: jira.buildURL('/filter')
+  }).then((jiraRes) => {
+  
+    let result = jiraRes.map(filter => {
+      return {
+        text: filter.name,
+        value: filter.id
+      }
+    })
+
+    httpRes.json(result)
+  })
+  
+})
+
+
+// Should return metrics based on input.
+app.post('/query', 
+  passport.authenticate(authenticationStrategy, { session: false }),
+  (httpReq, httpRes) => {
+
+  let result = []
+
+  // Convert proper formatted Grafana data into the Jira mess
+  let from = new Date(httpReq.body.range.from).toISOString().replace(/T/, ' ').replace(/\:([^:]*)$/, '')
+  let to = new Date(httpReq.body.range.to).toISOString().replace(/T/, ' ').replace(/\:([^:]*)$/, '')
+
+  let p = httpReq.body.targets.map(target => {
+
+    // Default jql with time range
+    let jql = [`created >= "${from}"`, `created <= "${to}"`]
+
+    // Additional jql for targets
+    if ( target.target ) {
+      jql.push(`filter = "${target.target}"`)
+    }
+
+    return jira.search.search({
+      jql: jql.join(' AND ')
+    }).then((jiraRes) => {
+
+      if (target.type == 'timeserie') {
+        
+        let datapoints = jiraRes.issues.map(issue => {
+          timestamp = Math.floor(new Date(issue.fields.created))
+          return [1, timestamp]
+        })
+
+        result.push({
+          target: target.target,
+          datapoints: datapoints
+        })
+
+      }
+      else if (target.type == 'table') {
+
+        let rows = jiraRes.issues.map(issue => {
+          return [
+            issue.key,
+            issue.fields.summary,
+            issue.fields.assignee ? issue.fields.assignee.displayName : '',
+            issue.fields.status ? issue.fields.status.name : '',
+            issue.fields.created
+          ]
+        })
+
+        result.push({
+          columns: [
+            { text: 'Key', 'type': 'string' },
+            { text: 'Summary', 'type': 'string' },
+            { text: 'Assignee', 'type': 'string' },
+            { text: 'Status', 'type': 'string' },
+            { text: 'Created', 'type': 'time' }
+          ],
+          type: 'table',
+          rows: rows
+        })
+      }
+
+
+    })
+  })
+
+  // Once all promises resolve, return result
+  Promise.all(p).then(() => {
+    httpRes.json(result)
+  })
+  
+})
+
+
+app.listen(3005)
+
+
+console.log('Server is listening to port 3005')
